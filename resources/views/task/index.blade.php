438e76a6df76893d2dad810017961213d9006df3
Rifky Syaripudin <rks@jiwalu.id>
2023-02-07 08:39:59 +0700

    first commit

---
+++ resources/views/task/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Task</div>
+                <table class="table table-bordered">
+                    <thead>
+                        <th>Nama</th>
+                        <th>Description</th>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
